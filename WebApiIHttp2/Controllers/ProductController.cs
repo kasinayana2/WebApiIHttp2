953951578141643a791e66b78440233b8c8c4880
kasi <kasinayana2@gmail.com>
2022-11-24 15:40:21 +0530

    Add project files.

---
+++ WebApiIHttp2/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiIHttp2.Models;
+
+namespace WebApiIHttp2.Controllers
+{
+    public class ProductController : ApiController
+    {
+        DatabaseConnection db;
+        public ProductController()
+        {
+            db = new DatabaseConnection();
+        }
+        [Route ("Product/GetAll")]
+        [AcceptVerbs("Get")]
+        public IEnumerable<Product> GetAll()
+        {
+            return db.Products.ToList();
+        }
+        [Route ("Product/Get-id/{id:int}")]
+        public Product GetById(int id)
+        {
+            return db.Products.Find(id);
+        }
+        [Route("Product/Get-name/{name:maxlength(20)}")]
+        public Product GetName(string name)
+        {
+            return db.Products.Where(p => p.ProductName == name).FirstOrDefault();
+        }
+        [HttpPost]
+        public void AddProduct(Product value)
+        {
+            db.Products.Add(value);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ WebApiIHttp2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiIHttp2.WebApiApplication" Language="C#" %>
